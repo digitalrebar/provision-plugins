f05feff0298e317b6d59e366b2569020b16d63c1
Michael Rice <michael@michaelrice.org>
2019-09-26 14:10:27 -0500

    Update image-builder to newer version of curtin
    Updates the image-builder plugin to use version 19.2 of curtin
    
    Signed-off-by: Michael Rice <michael@michaelrice.org>

--- cmds/image-deploy/get-curtin.sh
+++ cmds/image-deploy/get-curtin.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p embedded
 
-files=(libyaml-0.1.4-11.el7_0.x86_64.rpm PyYAML-3.10-11.el7.x86_64.rpm libtommath-0.42.0-6.el7.x86_64.rpm curtin-17.1-mbcache.tgz python-oauthlib-0.6.0-2.el7.noarch.rpm libtomcrypt-1.17-26.el7.x86_64.rpm python2-crypto-2.6.1-15.el7.x86_64.rpm dpkg-1.17.27-1.el7.x86_64.rpm)
+files=(libyaml-0.1.4-11.el7_0.x86_64.rpm PyYAML-3.10-11.el7.x86_64.rpm libtommath-0.42.0-6.el7.x86_64.rpm curtin-19.2-mbcache.tar.gz python-oauthlib-0.6.0-2.el7.noarch.rpm libtomcrypt-1.17-26.el7.x86_64.rpm python2-crypto-2.6.1-15.el7.x86_64.rpm dpkg-1.17.27-1.el7.x86_64.rpm jsonschema-3.0.2.tar.gz)
 for i in "${files[@]}"
 do
     if [[ ! -e embedded/$i ]] ; then
