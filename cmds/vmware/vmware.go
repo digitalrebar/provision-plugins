fe9b1366cebc42b6db86d2382cd35da8499495e7
Greg Althaus <galthaus@austin.rr.com>
2020-11-18 16:50:09 -0600

    build: Fix build break for a second

--- cmds/vmware/vmware.go
+++ cmds/vmware/vmware.go
@@ -2,7 +2,6 @@ package main
 
 //go:generate ./download-vibs.sh
 //go:generate sh -c "cd process ; go build -o ../process-content"
-//go:generate ./process-content
 //go:generate sh -c "cd content ; drpcli contents bundle ../content.go"
 //go:generate sh -c "cd content ; drpcli contents bundle ../content.yaml"
 //go:generate sh -c "drpcli contents document content.yaml > vmware.rst"
