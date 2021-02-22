9d6b2f5cae662b7750f06342f3f5067af2eff810
Greg Althaus <galthaus@austin.rr.com>
2021-02-21 23:17:10 -0600

    fix(image-deploy): fix curtin's bad assumptions about base OS for curtinhooks

--- cmds/image-deploy/get-curtin.sh
+++ cmds/image-deploy/get-curtin.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p embedded
 
-files=(libyaml-0.1.4-11.el7_0.x86_64.rpm PyYAML-3.10-11.el7.x86_64.rpm libtommath-0.42.0-6.el7.x86_64.rpm curtin-20.2-mbcache-dev.tar.gz python-oauthlib-0.6.0-2.el7.noarch.rpm libtomcrypt-1.17-26.el7.x86_64.rpm python2-crypto-2.6.1-15.el7.x86_64.rpm dpkg-1.17.27-1.el7.x86_64.rpm)
+files=(libyaml-0.1.4-11.el7_0.x86_64.rpm PyYAML-3.10-11.el7.x86_64.rpm libtommath-0.42.0-6.el7.x86_64.rpm curtin-20.2-mbcache-dev-curthooks.tgz python-oauthlib-0.6.0-2.el7.noarch.rpm libtomcrypt-1.17-26.el7.x86_64.rpm python2-crypto-2.6.1-15.el7.x86_64.rpm dpkg-1.17.27-1.el7.x86_64.rpm)
 for i in "${files[@]}"
 do
     if [[ ! -e embedded/$i ]] ; then
