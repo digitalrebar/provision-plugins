318423567fe506b1b511761c26a33e39956344a1
Greg Althaus <galthaus@austin.rr.com>
2021-06-05 13:48:46 -0500

    feat(raid,bios): add ppc64le versions

--- cmds/raid/build-drp-raid.sh
+++ cmds/raid/build-drp-raid.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir -p embedded
 cd drp-raid && GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o ../embedded/drp-raid.amd64.linux
+GOOS=linux GOARCH=ppc64le go build -ldflags "-s -w" -o ../embedded/drp-raid.ppc64le.linux
